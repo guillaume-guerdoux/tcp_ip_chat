4385b9e65ff6b70494e390738b5ee9d3035536e3
Guerdoux Guillaume <guillaume.guerdoux@student.ecp.fr>
2016-10-11 16:54:30 +0200

    Update thread_client_server.py

--- thread_client_server.py
+++ thread_client_server.py
@@ -15,7 +15,7 @@ class Chat_Server(threading.Thread):
 		self.type_of_thread = "SERVER"
 		self.clients_connected = []
 	def run(self):
-		PORT = 44441
+		PORT = 44443
 		self.main_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		#self.main_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.main_connection.bind((self.host,PORT))
@@ -34,6 +34,8 @@ class Chat_Server(threading.Thread):
 				pass
 			else:
 				for client in clients_to_read:
+					'''clients_to_send = self.clients_connected
+					clients_to_send.remove(client)'''
 					msg_received = client.recv(1024)
 					msg_received = msg_received.decode()
 					print(msg_received)
@@ -41,8 +43,14 @@ class Chat_Server(threading.Thread):
 						print("end of connection with client")
 						self.kill()
 
+
 	def send(self, message):
 		for client in self.clients_connected:
+			client.send(message.encode())
+
+	def send_to_list_of_clients(self, message, list_client):
+		print(list_client)
+		for client in list_client:
 			client.send(message.encode())
 
 	def kill(self):
@@ -58,7 +66,7 @@ class Chat_Client(threading.Thread):
 		self.running = 1
 		self.type_of_thread = "CLIENT"
 	def run(self):
-		PORT = 44441
+		PORT = 44443
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.connect((self.host, PORT))
 		print("Connection with server done")
@@ -109,9 +117,7 @@ class Text_Input(threading.Thread):
 					self.send_chat.kill()
 					self.kill()
 				else:
-					#text = text.encode()
-					# On envoie le message
-					self.send_chat.send(text)
+					self.send_chat.send(text)					
 			except:
 				Exception
 			time.sleep(0)
