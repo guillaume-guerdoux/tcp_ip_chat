9bf21be140c1c80e185a6dd9b27b86b23b78ad29
Sophie Moustard <SophieMstd@MacBook-Pro-de-Sophie-2.local>
2016-10-17 12:33:21 +0200

    add try and except

--- test_clients_file/client_file.py
+++ test_clients_file/client_file.py
@@ -1,20 +1,12 @@
 import socket
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "127.0.0.1"    
+host = "138.195.110.204"    
 port = 60002                
-connexion_avec_serveur.connect((host, port))
 
-<<<<<<< Updated upstream
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)             # Create a socket object
-host = '138.195.110.204'    # Get local machine name
-port = 44451                   # Reserve a port for your service.
-
-s.connect((host, port))
-s.send("Hello server!".encode())
-=======
-print("client connecté")
-connexion_avec_serveur.send(b"Hello server!")
->>>>>>> Stashed changes
+try:
+    connexion_avec_serveur.connect((host, port))
+except Exception, e:
+    alert("Something's wrong with %s. Exception type is %s" % (address, e))
 
 with open('received_file', 'wb') as f:
     print ('file opened')
