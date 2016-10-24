f2f501148d48a56a4bad229695e95797cadbd73c
Sophie Moustard <SophieMstd@MacBook-Pro-de-Sophie-2.local>
2016-10-24 12:00:24 +0200

    add comments

--- server_file.py
+++ server_file.py
@@ -1,6 +1,3 @@
-#TODO : ajouter droit d'acces 
-#choix du fichier
-
 import socket                   # Import socket module
 import select
 
@@ -15,26 +12,26 @@ connexion_principale.listen(5)                     # Now wait for client connect
 print('Server listening....')
 connexion_avec_client, infos_connexion = connexion_principale.accept()     # Establish connection with client.
 print('Got connection from', infos_connexion)
-filename=input('chemin du fichier : ')
+filename=input('chemin du fichier : ') #Select the right fie
 msg_send2 = "Envoi du fichier"
 connexion_avec_client.send(msg_send2.encode())
-msg_received2 = connexion_avec_client.recv(1024)
+msg_received2 = connexion_avec_client.recv(1024) #Wait for opened file on client file
 msg_received2 = msg_received2.decode()
 if msg_received2 == "file opened":
-	msg_send3 = "Go"
+	msg_send3 = "Go" #Send the file 
 	connexion_avec_client.send(msg_send3.encode())
-	f = open(filename,'rb')
+	f = open(filename,'rb') #Open the file in reading mode
 	l = f.read(1024)
 	while (l):
 		connexion_avec_client.send(l)
 		print('Sent ',repr(l))
 		l = f.read(1024)
-	msg_received3 = connexion_avec_client.recv(1024)
+	msg_received3 = connexion_avec_client.recv(1024) #Wait for the file to be successfully received
 	msg_received3 = msg_received3.decode()
 	print (msg_received3)
-	f.close()
+	f.close() #close the file 
 	print('Done sending')
-	connexion_avec_client.close()
+	connexion_avec_client.close()#close the connection
 	False
 
 
--- test_clients_file/client_file.py
+++ test_clients_file/client_file.py
@@ -9,22 +9,22 @@ filename = input("Quel nom veut tu donner au fichier?")
 
 data2 = connection_with_server.recv(1024).decode()
 if data2 == "Envoi du fichier" : 
-	with open(filename, 'wb') as f:
+	with open(filename, 'wb') as f:  #create the file
 		print ('file opened')
 		msg_send = "file opened"
 		connection_with_server.send(msg_send.encode())
 		data4 = connection_with_server.recv(1024).decode()
-		if data4 == "Go":
+		if data4 == "Go": #file reception started
 			while True:
 				data3 = connection_with_server.recv(1024)
 				print('receiving data...')
 				print('data=%s', (data3))
 				if not data3:
 					break
-				f.write(data3)
+				f.write(data3)#write the received data in the file
 				msg_sent2 = "fichier recu"
 				connection_with_server.send(msg_send.encode())
-			f.close()
+			f.close() #close the file
 			print('Successfully get the file')
-			connection_with_server.close()
+			connection_with_server.close() #close the connection
 			print('connection closed')
